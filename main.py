8ffcab940546b8e30fc0da36c02d0fcb15fcae2d
Husseinat <alihusseinat@gmail.com>
2018-07-01 12:41:39 -0300

    Added number of iterations on input and changed test case to suport it

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from gauss_jacobi import GaussJacobiBuilder
 
+#input of number of iterations
+maxIterations = int(input().strip())
+
 # input of solution array
 solution = [int(num) for num in input().strip().split(' ')]
 
@@ -12,5 +15,5 @@ except EOFError:
     pass
 
 # build and solve
-result = GaussJacobiBuilder.createSolver().withSolution(solution).withEquations(matrix).solve(20)
+result = GaussJacobiBuilder.createSolver().withSolution(solution).withEquations(matrix).solve(maxIterations)
 print(result)
